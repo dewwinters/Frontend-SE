295316c727ce7e9fdd0415ac7ac83c38afe86045
VietDH1004 <dohungviet.tdn.2004@gmail.com>
2024-11-28 15:54:43 +0700

    Sign Up page update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import HomePage from './Pages/HomePage/HomePage';
 import NavBar from './Components/Navbar/Navigation';
+import SignUp from './Pages/SignUpPage/SignUp';
 
 function App() {
   return (
     <div className="App">
-        <NavBar />
-        <HomePage />
+        <SignUp></SignUp>
     </div>
   );
 }
