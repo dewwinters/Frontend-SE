6fb6f55cd0dfa38bd3d53c8ee8e361bab0444643
Minh Tri <trihaminh2004@gmail.com>
2024-11-30 03:31:10 +0700

    add changes

--- frontend/src/Pages/HomePage/HomePage.js
+++ frontend/src/Pages/HomePage/HomePage.js
@@ -89,8 +89,54 @@ const HomePage = () => {
             <ChevronRightOutlinedIcon />
           </button>
         </div>
+        
       </div>
-    <Footer />
+        
+      {/* bắt đầu làm */}
+      <div className="item__box">
+        <div className="item__box__card">
+
+          <div className="item__box__card__title">
+            Score Black Friday Week deals
+            <div className="item__box__card__image">
+
+                <div className="item__box__card__block">
+                  <img className="item__box__card__block__image" src="https://images-na.ssl-images-amazon.com/images/G/01/DiscoTec/2024/BFCM/GW/Quad_Cards/BFCM_2024_772_BFW_CM_DQC_AmazonBasics_3D_1x_v3._SY116_CB541717183_.jpg" alt="image01" />
+                  <div className="item__box__card__block__text">
+                    Amazon Basics and more
+                  </div>
+                </div>
+                  
+                <div className="item__box__card__block">
+                  <img className="item__box__card__block__image" src="https://images-na.ssl-images-amazon.com/images/G/01/DiscoTec/2024/BFCM/GW/Quad_Cards/BFCM_2024_768_BFW_CM_DQC_HomeImprovement_2D_1x_v3._SY116_CB541717183_.jpg" alt="image02" />
+                  <div className="item__box__card__block__text">
+                    Home Improvement
+                  </div>
+                </div>
+
+                <div className="item__box__card__block">
+                  <img className="item__box__card__block__image" src="https://images-na.ssl-images-amazon.com/images/G/01/DiscoTec/2024/BFCM/GW/Quad_Cards/BFCM_2024_770_BFW_CM_DQC_Phone_Accessories_3B_1x_v3._SY116_CB541717183_.jpg" alt="image03" />
+                  <div className="item__box__card__block__text">
+                    Cell phones & accessories
+                  </div>
+                </div>
+
+                <div className="item__box__card__block">
+                  <img className="item__box__card__block__image" src="https://images-na.ssl-images-amazon.com/images/G/01/DiscoTec/2024/BFCM/GW/Quad_Cards/BFCM_2024_769_BFW_CM_DQC_Furniture_3A_1x_v3._SY116_CB541717183_.jpg" alt="image04" />
+                  <div className="item__box__card__block__text">
+                    Furniture
+                  </div>
+                </div>
+            </div>
+
+            <div className="item__box__card__block__seeMore">
+              See more
+            </div>
+          </div> 
+        </div>
+      </div>
+      
+      {/* <Footer /> */}
     </div>
   );
 };
