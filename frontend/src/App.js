3c4e9ddee5a24e40e6e30db903e55ac39bbf33be
Minh Tri <trihaminh2004@gmail.com>
2024-11-30 21:37:37 +0700

    add footer to product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,6 @@ import Product from './Pages/ProductPage/Product';
 function App() {
   return (
     <div className="App">
-      <NavBar/>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/Product' element={<Product/>}/>
