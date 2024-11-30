4ef2d7df65eb2c454ce99fecf8f7cddf59923dcb
Minh Tri <trihaminh2004@gmail.com>
2024-11-30 14:31:57 +0700

    homepage done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,8 +8,9 @@ function App() {
   return (
     <div className="App">
         {/* <SignUp></SignUp> */}
-        <HomePage />
         {/* <NavBar /> */}
+        <HomePage />
+        {/* <Footer /> */}
     </div>
   );
 }
